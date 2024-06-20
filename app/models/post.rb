27d0e20d0bc9fffc32b17e90704d7e632cb3bda9
hardow2011 <hardow2011@outlook.com>
2024-06-20 15:10:15 -0400

    models annotated

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,40 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  caption    :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  unique_id  :string           default(""), not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_unique_id  (unique_id) UNIQUE
+#  index_posts_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Post < ApplicationRecord
   require 'securerandom'
 
   belongs_to :user
 
-  before_validation :unique_identifier
+  before_validation :unique_id
 
-  validates :unique_identifier, format: { with: /^[a-zA-Z0-9]*$/ }, length: { is: 11 },
+  validates :unique_id, format: { with: /^[a-zA-Z0-9]*$/ }, length: { is: 11 },
             presence: true, uniqueness: true, allow_blank: false,
             allow_nil: false, on: :create
 
-  attr_readonly :unique_identifier
+  attr_readonly :unique_id
 
   private
 
-  def assign_unique_identifier
-    self.unique_identifier = SecureRandom.alphanumeric(11)
-    assign_unique_identifier if Post.exists?(unique_identifier: self.unique_identifier)
+  def assign_unique_id
+    self.unique_id = SecureRandom.alphanumeric(11)
+    assign_unique_id if Post.exists?(unique_id: self.unique_id)
   end
 end
