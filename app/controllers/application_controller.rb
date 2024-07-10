9977fa7c1debda5ad013a49102bb56d37984627c
hardow2011 <hardow2011@outlook.com>
2024-07-10 14:43:24 -0400

    feat: added post_path to replace community_post_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+  
   before_action :authenticate_user!, unless: :devise_controller?
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  include Rails.application.routes.url_helpers
+
+  def post_path(post)
+    community_post_path(post.community, post)
+  end
 end
