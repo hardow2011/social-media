72e87f745a5d2dbbcd0e1c19e044ac5f8cf68b76
hardow2011 <hardow2011@outlook.com>
2024-07-15 21:25:46 -0400

    feat: add user to comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_032311) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_011258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_032311) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "communities", force: :cascade do |t|
@@ -69,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_032311) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "communities"
