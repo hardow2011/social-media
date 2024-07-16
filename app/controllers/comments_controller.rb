e498605761c8dc891f2e851306cbd1dc2e81a286
hardow2011 <hardow2011@outlook.com>
2024-07-15 22:09:58 -0400

    refactor: simplify comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,11 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
+    @comment = Comment.new(comment_params)
 
     if @comment.save
-      redirect_to post_path(@post), notice: 'Comment was succesfully created.'
+      post = @comment.post
+      redirect_to post_path(post), notice: 'Comment was succesfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -38,7 +38,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id)
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 
   def set_comment
