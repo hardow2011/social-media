845186ed86ffc17018bdd2f9b9044194b3bb286c
hardow2011 <hardow2011@outlook.com>
2024-07-16 01:19:33 -0400

    feat: custom redirect_to after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-  
+
   before_action :authenticate_user!, unless: :devise_controller?
+
+  # TODO: create way to prevent redirects to certain pages
+
+  private
+
+  def after_sign_in_path_for(resource)
+    if params[:redirect_to].present?
+      store_location_for(resource, params[:redirect_to])
+    elsif request.original_url == new_session_url(:user)
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
 end
