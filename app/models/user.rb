7a44142c48d46531b06a6506c883d439fe02ae06
hardow2011 <hardow2011@outlook.com>
2024-07-24 13:35:44 -0400

    refactor: remove unnecesary line

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :delete_all
-  has_many :likes, dependent: :delete_all
   has_many :comments, dependent: :delete_all
 
   has_and_belongs_to_many :communities
